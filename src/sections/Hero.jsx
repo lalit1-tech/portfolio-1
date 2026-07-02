59ddc4b060b73f2095b0e7d5b0ff8a7f1d1cb2fe
lalit1-tech <149093576+lalit1-tech@users.noreply.github.com>
2026-07-02 21:53:14 +0530

    Update profile image source in Hero component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -124,7 +124,7 @@ export const Hero = () => {
               />
               <div className="relative glass rounded-3xl p-2 glow-border">
                 <img
-                  src="public/lalit_profile.png"
+                  src="https://drive.google.com/file/d/1R4vejUueQANu-VH2YVeKDMTiEgb-Bu0R/view?usp=drive_link"
                   alt="Lalit Meena"
                   className="w-full aspect-[4/5] object-cover rounded-2xl"
                 />
