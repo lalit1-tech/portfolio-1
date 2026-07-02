800735f2495ee02c50a1af04bb9053d39a880ada
lalit1-tech <149093576+lalit1-tech@users.noreply.github.com>
2026-07-02 19:31:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Qualifications } from "@/sections/Qualifications";
+import { Contact } from "@/sections/Contact";
+import { Footer } from "./layout/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Qualifications />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
